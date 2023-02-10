0f4b10cdffb72d55ee4e84cb788d37eab1b3c496
Daria-Misko <111460998+Daria-Misko@users.noreply.github.com>
2023-02-10 07:17:08 +0000

    Deploying to gh-pages from @ Daria-Misko/goit-js-hw-09v2@39ae5ca9d396d06d87ad48361e35d91f760dbcd2 🚀

---
+++ 01-color-switcher.e11bdf95.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");document.querySelector("body");t.addEventListener("click",(function(){n=setInterval((function(){return document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3),t.disabled=!0})),e.addEventListener("click",(function(){clearInterval(n),t.disabled=!1}));var n=null}();
+//# sourceMappingURL=01-color-switcher.e11bdf95.js.map
