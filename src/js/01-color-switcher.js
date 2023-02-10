39ae5ca9d396d06d87ad48361e35d91f760dbcd2
Daria-Misko <miskodaria.mail@gmail.com>
2023-02-10 09:16:38 +0200

    change 1st task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -8,9 +8,10 @@ buttonStop.addEventListener('click', blocColorBgChange)
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+let timerId = null;
 
 function colorBgChange() {
-	const timerId = setInterval(() => {
+	timerId = setInterval(() => {
 		return document.body.style.backgroundColor = getRandomHexColor();
 	}, 1000);
 	buttonStart.disabled = true;
