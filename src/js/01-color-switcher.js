5eb7f4e83867048c1da6b1afbdcc5f0eaa62bb2f
Daria-Misko <miskodaria.mail@gmail.com>
2023-02-10 09:16:56 +0200

    add space

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -8,6 +8,7 @@ buttonStop.addEventListener('click', blocColorBgChange)
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
 let timerId = null;
 
 function colorBgChange() {
