361fed83a319524da5cbcb7064f58aff9fcd7bdb
Daria-Misko <111460998+Daria-Misko@users.noreply.github.com>
2023-02-11 16:51:51 +0000

    Deploying to gh-pages from @ Daria-Misko/goit-js-hw-09v2@d2659400558a326f4407dac71315fa6a31bc5541 🚀

---
+++ 01-color-switcher.48bcabff.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");document.querySelector("body");t.addEventListener("click",(function(){n=setInterval((function(){return document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3),e.disabled=!1,t.disabled=!0})),e.addEventListener("click",(function(){clearInterval(n),e.disabled=!0,t.disabled=!1})),e.disabled=!0;var n=null}();
+//# sourceMappingURL=01-color-switcher.48bcabff.js.map
---
+++ 01-color-switcher.645746ac.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");document.querySelector("body");t.addEventListener("click",(function(){d=setInterval((()=>document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`),1e3),e.disabled=!1,t.disabled=!0})),e.addEventListener("click",(function(){clearInterval(d),e.disabled=!0,t.disabled=!1})),e.disabled=!0;let d=null;
+//# sourceMappingURL=01-color-switcher.645746ac.js.map
